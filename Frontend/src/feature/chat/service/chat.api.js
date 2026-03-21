f068e6a7fb473c726fec9961045a306244a96a4a
trushitdarji <trushitdarji55@gmail.com>
2026-03-21 21:14:14 +0530

    Edit some chat API service for improved code consistency and formatting

--- Frontend/src/feature/chat/service/chat.api.js
+++ Frontend/src/feature/chat/service/chat.api.js
@@ -1,26 +1,26 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
-    withCredentials:true
-})
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
 
-export async function sendMessage({message,chatId}){
-    const response = await api.post("/api/chats/message",{message,chatId})
-    return response.data
+export async function sendMessage({ message, chatId }) {
+  const response = await api.post("/api/chats/message", { message, chatId });
+  return response.data;
 }
 
-export async function getChats(){
-    const response = await api.get("/api/chats")
-    return response.data
+export async function getChats() {
+  const response = await api.get("/api/chats");
+  return response.data;
 }
 
-export async function getMessages(chatId){
-    const resposne = await api.get(`/api/chats/${chatId}/messages`)
-    return resposne.data
+export async function getMessages(chatId) {
+  const resposne = await api.get(`/api/chats/${chatId}/messages`);
+  return resposne.data;
 }
 
-export async function deleteChat(chatId){
-    const response = await api.delete(`/api/chats/delete/${chatId}`)
-    return response.data
+export async function deleteChat(chatId) {
+  const response = await api.delete(`/api/chats/delete/${chatId}`);
+  return response.data;
 }
